8b6a402414b204c96cef03cbcd921d69b0d5cdb0
jacobadam <jacobanevitt@gmail.com>
2021-01-05 20:16:16 -0800

    add css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import '../src/Components/NavBar';
-import NavBar from '../src/Components/NavBar';
+// import NavBar from "../src/Components/NavBar";
+// import HomePage from "../src/Components/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <h1>Homepage</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">column one</div>
+        <div className="col-6">column two</div>
+          <span>
+              <i className="fas fa-home"/>
+          </span>
+      </div> 
     </div>
   );
 }
