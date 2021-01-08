2653b8c5b27abed22f398d8db497e4d6dbb1034a
jacobadam <jacobanevitt@gmail.com>
2021-01-08 14:40:16 -0800

    create data file and image folder

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "The Last Of Us",
+    img: "img/the_last_of_us.jpg",
+    price: 40,
+    developer: "Naughty Dog",
+    info:
+      "The Last of Us is a 2013 action-adventure game developed by Naughty Dog and published by Sony Computer Entertainment. Players control Joel, a smuggler tasked with escorting a teenage girl, Ellie, across a post-apocalyptic United States. The Last of Us is played from a third-person perspective. Players use firearms and improvised weapons, and can use stealth to defend against hostile humans and cannibalistic creatures infected by a mutated fungus in the genus Cordyceps. In the online multiplayer mode, up to eight players engage in cooperative and competitive gameplay.",
+  },
+  {
+    id: 2,
+    title: "Banjo Kazooie",
+    img: "img/banjo_kazooie.png",
+    price: 60,
+    developer: "Rare",
+    info:
+      "Banjo-Kazooie is a series of video games developed by Rare. The games feature a male bear named Banjo and his friend, a large female red bird called Kazooie, who are both controlled by the player. Throughout the various games, they are tasked with thwarting the various evil schemes of a witch named Gruntilda. The first game, Banjo-Kazooie, was released in 1998 to critical acclaim, and was followed by three sequels and a spin-off racing game.",
+  },
+  {
+    id: 3,
+    title: "Grand Theft Auto: San Andreas",
+    img: "img/gta_sa.jpg",
+    price: 20,
+    developer: "Rockstar",
+    info:
+      "Grand Theft Auto: San Andreas is a 2004 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the seventh title in the Grand Theft Auto series, and the follow-up to the 2002 game Grand Theft Auto: Vice City. It was released in October 2004 for PlayStation 2, and in June 2005 for Microsoft Windows and Xbox. The game is set within an open world environment that players can explore and interact with at their leisure. The story follows former gangster Carl 'CJ' Johnson, who returns home after the death of his mother and is drawn back into his former gang and a life of crime while clashing with corrupt authorities, rival criminal syndicates, and other enemies. Carl's journey takes him across the fictional U.S. state of San Andreas, which is heavily based on California and Nevada.",
+  },
+];
