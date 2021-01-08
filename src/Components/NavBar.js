a814088d4981497be9db760c6ae7c7c1db705039
jacobadam <jacobanevitt@gmail.com>
2021-01-08 15:53:41 -0800

    stylise nav bar, add logo, add links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "../logo.png";
 
 class NavBar extends Component {
   render() {
-    return <nav className="navbar navbar-expand-sm navbar-dark px-sm-5"></nav>;
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" classname="navbar-brand" />
+        </Link>
+        <ul className="ul navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button><i className="fas fa-cart-plus">Cart</i></button>
+        </Link>
+      </nav>
+    );
   }
 }
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,7 +4,7 @@ class Products extends Component {
   render() {
     return (
       <div>
-        <h1>Products</h1>
+        <h1>Products Page</h1>
       </div>
     );
   }
